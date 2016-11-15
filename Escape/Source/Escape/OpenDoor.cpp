7d5280fc81d812df5b75abaf555d8b7111eb4144
SergeevAlexander <arzhanok@mail.ru>
2016-11-15 16:54:02 +0300

    Room Layout

---
+++ Escape/Source/Escape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Escape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find Owning Actor
+	AActor* Onwer = GetOwner();
+
+	// Create a Rotator
+	FRotator NewRotation = FRotator::ZeroRotator;
+
+	// Set the door rotation
+	if (Onwer)
+	{
+		Onwer->SetActorRotation(NewRotation);
+	}
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
