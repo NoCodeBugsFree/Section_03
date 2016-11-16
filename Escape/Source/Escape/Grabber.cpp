0c4b86ad785b6c80abec357d8efeb7ff1b86e799
SergeevAlexander <arzhanok@mail.ru>
2016-11-16 13:01:33 +0300

    AController::GetPlayerViewPoint

--- Escape/Source/Escape/Grabber.cpp
+++ Escape/Source/Escape/Grabber.cpp
@@ -3,6 +3,7 @@
 #include "Escape.h"
 #include "Grabber.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -33,8 +34,18 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 
 	// Get the player viewpoint
 
+	// AController::GetPlayerViewPoint
+	// Returns Player's Point of View For the AI this means the Pawn's 'Eyes'
+	// ViewPoint For a Human player, this means the Camera's ViewPoint 
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	// TODO log out to test
+	UE_LOG(LogTemp, Error, TEXT("Location = %s  Rotation = %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+
 	// Raycast out to reach distance
 
-	// 
+	// See what we hit
 }
 
